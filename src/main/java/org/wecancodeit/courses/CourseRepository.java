a73cfe8f31ae09e88cf680d105e3edc0a6a0d6f4
bryancharleslong <bryancharleslong@gmail.com>
2019-02-08 11:34:31 -0500

    tested and wired Mvc

--- src/main/java/org/wecancodeit/courses/CourseRepository.java
+++ src/main/java/org/wecancodeit/courses/CourseRepository.java
@@ -10,7 +10,18 @@ import org.springframework.stereotype.Repository;
 public class CourseRepository {
 
 	private Map<Long, Course> courseList = new HashMap<>();
+	
+	public CourseRepository() {
+		Course java = new Course(1L, "Java", "Java description");
+		Course javascript = new Course(2L, "Javascript", "Javascript descripiton");
+		Course spring = new Course(3L, "Spring", "Spring description");
+		
+		courseList.put(java.getId(), java);
+		courseList.put(javascript.getId(), javascript);
+		courseList.put(spring.getId(), spring);
+	}
 
+	//Constructor for testing purposes
 	public CourseRepository(Course... courses) {
 		for (Course course : courses) {
 			courseList.put(course.getId(), course);
